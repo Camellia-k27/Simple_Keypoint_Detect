88d297a54a8bcaf1fee0a66a1ba4c4121b99c100
xxBLUECATxx <hyh011025@gmail.com>
2024-06-05 10:52:41 +0800

    Labeller Message

--- labeller/labeller.py
+++ labeller/labeller.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template, request, jsonify
+import os
+import logging
 
 app = Flask(__name__)
-
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
 @app.route('/')
 def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+
+    app.run(debug=False)
